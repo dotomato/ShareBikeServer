49859d9dc0dfcb3c6c2129f53ce95c6c10d76ec7
chen <dotomato@163.com>
2017-11-27 21:58:04 +0800

    共享单车：扫描器端，完成

--- Server.py
+++ Server.py
@@ -13,11 +13,48 @@ VERSION = 'v0.01'
 APIURL = '/api/' + VERSION
 
 
+class CarState:
+
+    def __init__(self, id):
+        self.id = id
+        self.is_return = True
+
+
+carlist = []
+
+
+def get_car(id):
+    for car in carlist:
+        if car.id == id:
+            return car
+        else:
+            return None
+
+
+def new_car(id):
+    if get_car(id) is not None:
+        return get_car(id)
+    else:
+        car = CarState(id)
+        carlist.append(car)
+        return car
+
+
 @app.route(APIURL + '/borrow_car', methods=['POST'])
 def borrow_car():
     data = request.get_data()
     body = json.loads(data)
-    result = {'result': 'success'}
+    id = body['id']
+    print id
+    car = get_car(id)
+    if car is None:
+        car = new_car(id)
+    if not car.is_return:
+        result = {'result': 'failed'}
+    else:
+        result = {'result': 'success'}
+        car.is_return = False
+        print 'car has been rented ,id=%s' % id
     return make_response(jsonify(result), 200)
 
 
@@ -25,7 +62,28 @@ def borrow_car():
 def return_car():
     data = request.get_data()
     body = json.loads(data)
-    result = {'result': 'success'}
+    id = body['id']
+    print id
+    car = get_car(id)
+    if car is None:
+        result = {'result': 'failed'}
+    else:
+        result = {'result': 'success'}
+        car.is_return = True
+        print 'car has been returned ,id=%s' % id
+    return make_response(jsonify(result), 200)
+
+
+@app.route(APIURL + '/is_return', methods=['POST'])
+def is_return():
+    data = request.get_data()
+    body = json.loads(data)
+    id = body['id']
+    car = get_car(id)
+    if car is None or not car.is_return:
+        result = {'result': 'not return'}
+    else:
+        result = {'result': 'return'}
     return make_response(jsonify(result), 200)
 
 
